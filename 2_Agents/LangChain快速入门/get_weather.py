be1bb115244e524e546817fba2def03e9877f47f
kim hugo <hugoiee@icloud.com>
2026-01-04 22:27:34 +0800

    feat(main): ✨️跑通 LangChain or Gemini

--- 2_Agents/LangChain快速入门/get_weather.py
+++ 2_Agents/LangChain快速入门/get_weather.py
@@ -1,4 +1,4 @@
-from http.client import responses
+import os
 
 import requests
 from dotenv import load_dotenv
@@ -6,28 +6,36 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+# 配置 API 密钥
 load_dotenv()
-
-model = ChatGoogleGenerativeAI(
-    model="gemini-3-flash-preview",
-)
+api_key = os.getenv("GEMINI_API_KEY")
 
 @tool('get_weather', description="返回查询的天气", return_direct=False)
 def get_weather(city: str):
-    response = requests.get(f'https://wttr.in/{city}?format=j1')
-    return response.json()
+    return "未知城市，暂无数据 (模拟返回: 晴, 22°C)"
 
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=1.0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
 
 agent = create_agent(
-    model=model,
-    tools=[get_weather],
-    system_prompt="你是一个充满幽默的天气助手",
+    model,
+    tools = [get_weather],
+    system_prompt = "你是一个乐于助人的天气助手。如果用户询问天气，请调用 get_weather 工具告诉她"
 )
 
 response = agent.invoke({
-    "message": [
-        {"role": "user", "content": "New York的天气怎么样"}
+    "messages" : [
+        {
+            "role": "user",
+            "content": "纽约的天气怎么样？"
+        }
     ]
 })
 
-print(response["message"][-1])
+print(response)
+print(response["messages"][-1])
